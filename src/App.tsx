d62346cc2acc1b5b96587efb891b12836fed7846
Jhofran Briceño <jhofran.dev@outlook.com>
2025-01-14 08:47:26 -0500

    Creando el menu y sus items

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
+import MenuItem from "./components/MenuItem";
+import { menuItems } from "./data/db"
+
 function App() {
+
+  console.log(menuItems);
+
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <header className=" bg-teal-400 py-5">
+        <h1 className=" text-center text-4xl font-black">Calculadora de Propina y Consumo</h1>
+      </header>
+
+      <main className=" max-w-7xl mx-auto py-20 grid md:grid-cols-2">
+        <div>
+          <h2>Menú</h2>
+          {menuItems.map(item => (
+            <MenuItem key={item.id} />
+          ))}
+        </div>
+
+        <div>
+          <h2>Consumo</h2>
+        </div>
+      </main>
     </>
   )
 }
